e0f26404e5ff110ac40ae73c213085e08487f0d4
LautaroAcosta1 <acostalauty765@gmail.com>
2024-05-14 03:37:19 -0300

    Se agrega el proyecto.

---
+++ ProyectoBaseTP/src/juego/Princesa.java
@@ -0,0 +1,17 @@
+package juego;
+
+public class Princesa{
+	int vidas;
+	
+    // constructor
+    public Princesa(int vidas) {
+    	this.vidas = vidas;
+    }
+    
+    public static void main(String[] args) {
+    	
+    	
+    	
+    }
+    
+}
